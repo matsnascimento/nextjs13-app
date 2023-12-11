938a0215892152fde7d2d3cabd0e96fb6dc36984
Matheus Cardoso do Nascimento <math.tcl@gmail.com>
2023-12-10 21:19:15 -0300

    First Deploy

--- prisma/category.sql
+++ prisma/category.sql
@@ -1,18 +1,18 @@
-/*
--- Query: SELECT * FROM prompt_hub.CategoryPrompt
--- Date: 2023-12-10 19:34
-*/
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzu0k2q00001001d1iwjugr','Inteligência Artifical','tb/TbBrandOpenai');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzwvtxz0000141672gdhwra','Copywriting','tb/TbWriting');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzwy24n00011416323bslmr','Finanças','tb/TbCoin');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzwyool000214164ixa05v4','Recursos Humanos','tb/TbUsersGroup');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzwz6ng00031416wc09duy4','Aprendizagem e Educação','tb/TbBooks');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzwzmh400041416xpzi4jdk','Gestão','tb/TbBuilding');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzx03pv0005141638ru9pkr','Marketing e Vendas','tb/TbTargetArrow');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzx0u5f00061416o40grquf','Produtividade','tb/TbListCheck');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzx1enz000714166py79v0y','Programação','tb/TbCode');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzx2dr20008141657m0yacu','SEO','tb/TbWorldSearch');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzx3djd00091416ifkorwr7','Social Media','tb/TbShare');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzx3v9n000a14161wqhn0g8','Esporte e Fitness','tb/TbRun');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzx67qp000b1416hja71gyi','UI/UX Design','lu/LuPencilRuler');
-INSERT INTO `CategoryPrompt` (`id`,`content`,`iconName`) VALUES ('clpzx6t3y000c14161lk72uo7','Outros','ci/CiCircleMore');
+{
+    "CategoryPrompt" : {
+        "Inteligência Artifical"   : "tb/TbBrandOpenai" ,  
+        "Copywriting"              : "tb/TbWriting"     ,  
+        "Finanças"                 : "tb/TbCoin"        ,  
+        "Recursos Humanos"         : "tb/TbUsersGroup"  ,  
+        "Aprendizagem e Educação"  : "tb/TbBooks"       ,  
+        "Gestão"                   : "tb/TbBuilding"    ,  
+        "Marketing e Vendas"       : "tb/TbTargetArrow" ,  
+        "Produtividade"            : "tb/TbListCheck"   ,  
+        "Programação"              : "tb/TbCode"        ,  
+        "SEO"                      : "tb/TbWorldSearch" ,  
+        "Social Media"             : "tb/TbShare"       ,  
+        "Esporte e Fitness"        : "tb/TbRun"         ,  
+        "UI/UX Design"             : "lu/LuPencilRuler" ,  
+        "Outros"                   : "ci/CiCircleMore"     
+    }
+}
